a23eaa3c6d7fbd85b4647fe3754255f2b28a4db3
smk <apps@entersoft.in>
2018-03-14 03:00:46 +0530

    V15: Month change code and dynamic itemlist => Dcr added and some css changes

--- app/controllers/dcrs_controller.rb
+++ app/controllers/dcrs_controller.rb
@@ -2,9 +2,39 @@ class DcrsController < ApplicationController
 	require 'date'
   def index
   	if params[:id]
-  	 @dcrs = Dcr.where(:itemlist_id => params[:id])
-  	 @dcr = Dcr.where(:date => Date.today.strftime("%m-%d-%Y").to_s, :itemlist_id => params[:id]).last
-  	 @itemlist = Itemlist.find(params[:id])
+        @itemlist = Itemlist.find_by_id(params[:id])
+        @dcrs = Dcr.where(:itemlist_id => params[:id], :month =>Date.today.strftime("%B"))
+        @dcr = Dcr.where(:date => Date.today.strftime("%m-%d-%Y").to_s, :itemlist_id => params[:id]).last
+        date = Date.today
+        predate = date << 1
+        @predata = Dcr.where(:month => predate.strftime("%B").to_s, :itemlist_id => params[:id]).last
+        if @dcr.blank?
+            date = Date.today
+            predate = date << 1
+            itemdata = Itemlist.find_by_id(params[:id])
+            @predata = Dcr.where(:month => predate.strftime("%B").to_s, :itemlist_id => params[:id]).last
+            if @predata.blank?
+                precumrecqty = itemdata.cumrecqtystart
+                precumconqty = itemdata.cumconqtystart
+                prebalatsite = itemdata.balatsitestart
+            else
+                precumrecqty = @predata.cumrecqtytotal
+                precumconqty = @predata.cumconqtytotal
+                prebalatsite = @predata.balatsitetotal
+            end
+            month_date = (Date.today - Date.today.mday + 1)
+            no = Date.new(date.strftime("%Y").to_i, date.mon, -1).day
+            (0..no-1).each do |n|
+                Dcr.create(:itemlist_id => params[:id], :stockworkorder_id => @itemlist.stockworkorder_id,
+                :date => (month_date+n).strftime("%m-%d-%Y"), :cumrecqty => precumrecqty,
+                :recqty => 0,:cumconqty=>precumconqty,:cons=>0,:balatsite=>prebalatsite,
+                :month=>date.strftime("%B"),:year => date.strftime("%Y"),:premonthbal=>0,
+                :presentbal=>0,:remarks=>"",:cumrecqtytotal =>precumrecqty ,
+                :cumconqtytotal =>precumconqty , :balatsitetotal =>prebalatsite )
+            end
+        end
+        @dcr = Dcr.where(:date => Date.today.strftime("%m-%d-%Y").to_s, :itemlist_id => params[:id]).last
+        @stockworkorder = Stockworkorder.find_by_id(@itemlist.stockworkorder_id)
     else
       @dcrs = Dcr.all
     end
@@ -42,13 +72,13 @@ class DcrsController < ApplicationController
         puts d.strftime("%m-%d-%Y")
         dc = Dcr.where(:date =>d.strftime("%m-%d-%Y"),:itemlist_id => dcr_params["itemlist_id"]).last
         dc.update_attributes(:cumrecqtytotal => cumrecqtytotal, :cumconqtytotal => cumconqtytotal,
-         :balatsitetotal=>cumconqtytotal,
+         :balatsitetotal=>balatsitetotal,
          :cumrecqty => cumrecqtytotal,:cumconqty => cumconqtytotal,:balatsite=>balatsitetotal)
       end
   	
-    	redirect_back fallback_location: root_path
+    	redirect_back fallback_location: root_path, :notice => "updated"
   	else
-  		redirect_back fallback_location: root_path
+  		redirect_back fallback_location: root_path, :alert => "Error: Please try again."
   	end
   end
 
